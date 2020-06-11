53d091dce136e6ddb51ccc2f89ed79dcd6ccc857
Ben Lewin <blewin1@gmail.com>
2020-06-11 15:25:20 -0400

    Added fonts, changed colors, more work on overlay

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,15 @@ $(() => {
 
     const projectSheetAsJSON = "https://spreadsheets.google.com/feeds/list/13HLU_ZBMDyL83EBfluhYT3sF_FEpLV2c97ypxa_pQec/od6/public/values?alt=json"
 
+    //open and close nav when clicking menu icon
     $('.menu-icon').click(function (e) { 
         e.preventDefault();
         $('.menu-items').toggleClass('menu-items-closed');
+    });
+
+    //close menu after clicking menu option
+    $('nav a').click(function (e) { 
+        $('.menu-items').addClass('menu-items-closed');
     });
 
     // <img src="${projectInfo.image}" alt="picture of site">
@@ -13,7 +19,9 @@ $(() => {
     const addCard = (projectInfo) => {
         let $card =
         $(`<div class="project-card">
-            <h3>${projectInfo.title}</h3>
+            <div class="project-title">
+                <h3>${projectInfo.title}</h3>
+            </div>
             <div class="project-overlay">
                 <p>${projectInfo.description}</p>
                 <a href="${projectInfo.url}"><button type="button" class="btn btn-info">View Website</button></a>
@@ -28,19 +36,19 @@ $(() => {
     const renderProjects = (projectsArr) => {
         projectsArr.forEach(project => {
             addCard(project);
-    
+            console.log(project)
         });
     }
 
     $.ajax({
         url: projectSheetAsJSON
     }).then(data => {
-        console.log('aaa', data.feed.entry[0].title.$t)
+        console.log('aaa', data.feed.entry[0].gsx$liveurl)
         const projects = data.feed.entry.map(project => {
             return {
                 title: project.gsx$title.$t,
                 image: project.gsx$image.$t,
-                url: project.gsx$url.$t,
+                url: project.gsx$liveurl.$t,
                 description: project.gsx$description.$t
             }
         })
